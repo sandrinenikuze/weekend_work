3c1dbe86aa8a647471bfb47d554bd86f6fdfe623
Sandrine Nikuze <nikuzesandrine@yahoo.fr>
2017-01-16 17:43:30 +0200

    Classes

--- oop/rectangle.rb
+++ oop/rectangle.rb
@@ -1,6 +1,21 @@
 # Create a Rectangle class with readable width and height attributes
 # and a method to calculate its area.
+class Rectangle 
+  def initialize (width,height) 
+    @width = width
+    @height = height
+  end
 
+  def width
+    @width
+  end 
+  def height
+    @height
+  end
+  def area
+   area = @height*@width
+  end 
+end 
 
 
 # Driver code - don't touch anything below this line.
